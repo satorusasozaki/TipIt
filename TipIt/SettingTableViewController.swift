82e70d0aa3ef0144b377729a0fbc1ffe1a515c1f
satorusasozaki <sasozakisatoru@gmail.com>
2016-08-27 23:54:15 -0700

    Slider clean up

--- TipIt/SettingTableViewController.swift
+++ TipIt/SettingTableViewController.swift
@@ -15,32 +15,54 @@ class SettingTableViewController: UITableViewController {
     @IBOutlet weak var changerMinCell: ChangerCell!
     
     var ud: NSUserDefaults?
+    var tipViewController: TipViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         ud = NSUserDefaults.standardUserDefaults()
-
+        tipViewController = navigationController?.viewControllers[0] as? TipViewController
         setupPercentCells()
     }
 
     func setupPercentCells() {
         let percents = ud?.objectForKey("percents") as! [Double]
         
-        let maxPercent = percents[2]
+        let maxPercent = Float(percents[2] * 100)
         changerMaxCell.nameLabel.text = "Max"
-        changerMaxCell.percentSlider.setValue(Float(maxPercent)*100, animated: true)
-        changerMaxCell.percentLabel.text = "\(String(format: "%.0f", maxPercent*100))%"
+        changerMaxCell.percentSlider.setValue(maxPercent, animated: true)
+        changerMaxCell.percentLabel.text = "\(String(format: "%.0f", maxPercent))%"
         changerMaxCell.percentSlider.addTarget(self, action: #selector(addPercentToUD), forControlEvents: UIControlEvents.TouchUpInside)
+ 
+        let midPercent = Float(percents[1] * 100)
+        changerMidCell.nameLabel.text = "Mid"
+        changerMidCell.percentSlider.setValue(midPercent, animated: true)
+        changerMidCell.percentLabel.text = "\(String(format: "%.0f", midPercent))%"
+        changerMidCell.percentSlider.addTarget(self, action: #selector(addPercentToUD), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        let minPercent = Float(percents[0] * 100)
+        changerMinCell.nameLabel.text = "Min"
+        changerMinCell.percentSlider.setValue(minPercent, animated: true)
+        changerMinCell.percentLabel.text = "\(String(format: "%.0f", minPercent))%"
+        changerMinCell.percentSlider.addTarget(self, action: #selector(addPercentToUD), forControlEvents: UIControlEvents.TouchUpInside)
     }
     
     func addPercentToUD(sender: UISlider) {
+        var percents = ud?.objectForKey("percents") as! [Double]
         if (sender == changerMaxCell.percentSlider) {
-            var percents = ud?.objectForKey("percents") as! [Double]
             let sliderValue = Double(sender.value / 100)
             percents[2] = round(100 * sliderValue) / 100
             ud?.setObject(percents, forKey: "percents")
+        } else if (sender == changerMidCell.percentSlider) {
+            let sliderValue = Double(sender.value / 100)
+            percents[1] = round(100 * sliderValue) / 100
+            ud?.setObject(percents, forKey: "percents")
+        } else {
+            let sliderValue = Double(sender.value / 100)
+            percents[0] = round(100 * sliderValue) / 100
+            ud?.setObject(percents, forKey: "percents")
         }
-        let tipVC = navigationController?.viewControllers[0] as! TipViewController
-        tipVC.redraw()
+        tipViewController!.redraw()
     }
+    
+    // switch
 }
