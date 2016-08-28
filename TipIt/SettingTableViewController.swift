39fea7cae025766cc87defda7fa82b4f1f5c3f29
satorusasozaki <sasozakisatoru@gmail.com>
2016-08-27 23:45:35 -0700

    Redraw tipVC from settingVC

--- TipIt/SettingTableViewController.swift
+++ TipIt/SettingTableViewController.swift
@@ -26,7 +26,7 @@ class SettingTableViewController: UITableViewController {
     func setupPercentCells() {
         let percents = ud?.objectForKey("percents") as! [Double]
         
-        let maxPercent = percents[0]
+        let maxPercent = percents[2]
         changerMaxCell.nameLabel.text = "Max"
         changerMaxCell.percentSlider.setValue(Float(maxPercent)*100, animated: true)
         changerMaxCell.percentLabel.text = "\(String(format: "%.0f", maxPercent*100))%"
@@ -37,83 +37,10 @@ class SettingTableViewController: UITableViewController {
         if (sender == changerMaxCell.percentSlider) {
             var percents = ud?.objectForKey("percents") as! [Double]
             let sliderValue = Double(sender.value / 100)
-            percents[0] = round(100 * sliderValue) / 100
-            
+            percents[2] = round(100 * sliderValue) / 100
             ud?.setObject(percents, forKey: "percents")
         }
+        let tipVC = navigationController?.viewControllers[0] as! TipViewController
+        tipVC.redraw()
     }
-
-    
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//
-//    // MARK: - Table view data source
-//
-//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-//
-//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 0
-//    }
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
